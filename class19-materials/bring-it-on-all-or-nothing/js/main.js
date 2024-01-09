87b5b93dce003eecf41f89c21b467c2461be86d9
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-01-09 18:03:54 +0100

    review some loop exercises

--- class19-materials/bring-it-on-all-or-nothing/js/main.js
+++ class19-materials/bring-it-on-all-or-nothing/js/main.js
@@ -1,12 +1,12 @@
 // *Variables*
 // Declare a variable, assign it a boolean, and alert the value
-let varia = true
-alert(varia)
+let bool = true;
+alert(bool)
 
 // Declare a variable, reassign it to your favorite color, and console log the value
-let varia2 = "red";
-varia2 = "yellow";
-console.log(varia2)
+let favColor = "yellow";
+favColor = "white"
+console.log(favColor)
 
 // *Functions*
 // Create a function that takes in 4 numbers and returns the sum of the first 3 numbers 
@@ -14,27 +14,28 @@ console.log(varia2)
 
 function takesFour(n1, n2, n3, n4) {
     let operate = (n1 + n2 + n3) / n4;
-    return operate
+    return operate;
 }
 
-takesFour(5, 10, 2, 3)
+takesFour(10,20,30,5)
+
 
 // Create a function that takes in 2 numbers. Console log the first number to the power
 // of the second. Call the function.
 
-function takesTwo(n1, n2) {
-    let power = Math.pow(n1, n2)
-    console.log(power)
+function takesTwo (n1, n2) {
+    let pow = Math.pow(n1, n2);
+    console.log(pow)
 }
 
-takesTwo(3, 2)
+takesTwo(4,2)
 
 // *Conditionals*
 // Create a function that takes in a boolean and a string. If the boolean is true, 
 // alert the string. If the boolean is false, console log the string
 
-function weird(bool, str) {
-    if (bool) {                // no hay que poner "if (bool === true)" porque bool va a ser true o false, dado que es un boolean.
+function weird (bool, str) {
+    if (bool) {    // Es lo mismo que haber puesto (bool === true)
         alert(str)
     }
     else {
@@ -44,12 +45,12 @@ function weird(bool, str) {
 
 // OTRA FORMA DE HACERLO:
 
-// bool ? alert(srt) : console.log(str) 
+// bool ? alert(str) : console.log(str) 
 // Si bool es true hace lo que está a la izquierda de ":", si no hace lo que está a la derecha de ":"
 
 // FORMA MÁS CONTRAÍDA AÚN DE TODA LA FUNCIÓN:
 
-// const weird = (bool, str) => b ? alert (str) : console.log(str)
+// const weird = (bool, str) => bool ? alert (str) : console.log(str)
 
 
 //*Loops*
@@ -58,19 +59,19 @@ function weird(bool, str) {
 // if the number is divisible by 5 log "buzz" instead of the number, 
 // and if the number is divisible by 3 and 5 log "fizzbuzz" instead of that number
 
-function crazy(n1) {
-    for (let i = 1; i <= n1; i++) {
-        if (i % 3 === 0 && i % 5 === 0){
+function trying (n1) {
+    for (let i = 1; i <= n1; i++){
+        if(n1 % 3 === 0 && n1 % 5 === 0) {
             console.log("fizzbuzz")
         }
-        else if (i % 3 === 0) {
+        else if(n1 % 3 === 0) {
             console.log("fizz")
         }
-        else if (i % 5 === 0) {
+        else if(n1 % 5 === 0) {
             console.log("buzz")
         }
-        else {
-            console.log(i)
+        else{
+            console.log(n1)
         }
     }
 }
