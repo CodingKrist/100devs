f3bcd1e0793fc4fddb41390ec350cecda3f6f234
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-01-10 17:41:56 +0100

    review arrays and exercises

--- class19-materials/arrays-fight-to-the-finish/js/main.js
+++ class19-materials/arrays-fight-to-the-finish/js/main.js
@@ -1,8 +1,11 @@
 //Create an array of movie titles. Loop through the array and each element to the h2.
 
-let movies = ["Lion King","Aladdin","Inside Out"]
+let movies = ["Lion King", "Aladdin", "Interestellar"]
+for(let i = 0; i < movies.length; i++) {
+    document.querySelector("h2").innerText += movies[i]
+}
 
-movies.forEach((x, i) => document.querySelector("h2").innerText += x)
+
 
 // for(i = 0; i < movies.length; i++) {
 //     document.querySelector("h2").innerText += movies[i]
@@ -11,20 +14,21 @@ movies.forEach((x, i) => document.querySelector("h2").innerText += x)
 //Create an array of numbers. 
 // Loop through the array and add three to each number and replace the old number.
 
-let numbers = [2, 4, 6]
-numbers.forEach((item, index) => {  //es igual que poner forEach((x, i) => ...)
-    numbers[index] = item + 3
+let numbers = [10, 20, 30]
+numbers.forEach((x,i) => {
+    numbers[i] = numbers[i] + 3  //Tambien puedo poner: numbers[i] = x + 3
 })
 
 //Find the average of all the numbers from question two
 
-let sum = 0
+let sum = 0;
+
+for(let i = 0; i < numbers.length; i++){
+    sum += numbers[i]
+}
 
 // numbers.forEach((x) ==> sum+=x)
 // let average = x / numbers.length
 
-for(let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-})
-
 let average = sum / numbers.length
+
--- class19-materials/arrays-in-it-to-win-it/js/main.js
+++ class19-materials/arrays-in-it-to-win-it/js/main.js
@@ -1,17 +1,17 @@
 //Create an array of movies with at least three movies.
 
-let movies = ["Aladdin","Lion King","Inside Out"]
+let favMovies = ["Lion King", "Aladdin", "Interestellar"]
 
 //Using the array from above, store the first movie in a variable
 
-let favorite = movies[0]
+let firstMovie = favMovies[0]
 
 //Get the length of the original array and store it in a new variable
 
-let large = movies.length
+let lengthVar = favMovies.length
 
 //Get the last element in that array and store it in a new variable.
 // What if your array was really large and you didn't know the last index? 
 // Would your solution still work?
 
-let lastmov = movies[movies.length - 1]
+let lastElement = favMovies[favMovies.length -1]
