266f5009c2e56d27232896a582c8cde5dbdc538b
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-01-16 11:14:24 +0100

    some Arrays practice

--- class20-materials /review-in-paradise-after-paradise/js/main.js
+++ class20-materials /review-in-paradise-after-paradise/js/main.js
@@ -1,1 +1,15 @@
-// Create a function that takes in an array. If the first number, is less than the last number, alert "Hi". If the first number is greater than the last number, alert "Bye". If they are equal, alert "We close in an hour".
+// Create a function that takes in an array. 
+//If the first number, is less than the last number, alert "Hi". 
+//If the first number is greater than the last number, alert "Bye". 
+//If they are equal, alert "We close in an hour".
+
+function superArray(array) {
+        
+    if(array[0] < array[array.lenght - 1]) {
+        alert("Hi")
+    }else if (array > array[array.lenght - 1]{
+        alert("Bye")
+    }else {
+        alert("We close in an hour")
+    }
+}
--- class20-materials /review-summer-games/js/main.js
+++ class20-materials /review-summer-games/js/main.js
@@ -1,1 +1,12 @@
-//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+//Create a function that takes in an array of numbers. 
+//Multiply each number together and alert the product. 
+
+function arrNum(arr) {
+    let start = 1  //no es 0 porque todo lo que multipliquemos por 0 será 0 siempre.
+    arr.forEach(x => start *= x) // igual que poner arr.forEach((x) => start  = start * x)
+    alert(start)
+
+}
+
+//call
+arrNum([10,2,3])    
