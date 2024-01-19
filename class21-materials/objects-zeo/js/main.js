6dc1a47d3c131505603b472357e72b1815318c14
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-01-19 18:01:06 +0100

    starting with objects

--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,20 @@
 //Create a stopwatch object that has four properties and three methods
+
+let stopwatch = {}
+
+stopwatch.brand = "Nike"
+stopwatch.size = "medium"
+stopwatch.shape = "round"
+stopwatch.color = "black"
+
+stopwatch.startButton = function() {
+    console.log("Starting!!")
+}
+
+stopwatch.time = function(time) {
+    console.log(`the time is ${time}`)
+}
+
+stopwatch.sayBrand = function() {
+    console.log(stopwatch.brand)
+}   
