9d4c2d72cdffe6a04bbc9a080e39e0f0855a3e8c
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-01-25 21:24:06 +0100

    practising split, reverse and join

--- class22-materials/space-review/js/main.js
+++ class22-materials/space-review/js/main.js
@@ -12,8 +12,52 @@ console.log(sum)
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
 
+
+
+function squared(arr) {
+
+    let squ = arr.map( (x) => x = Math.pow(x,2) )
+    return squ
+}
+
+console.log(squared([2, 5, 10, 15, 20]))
+
+
 //Create a function that takes string
 //Print the reverse of that string to the console
 
+function takesStr(str) {
+
+    let rev = str.split(""); // lo divide en una Array: ['h', 'e', 'l', 'l', 'o']
+    rev = rev.reverse(); // convierte la array en una array inversa: ['o', 'l', 'l', 'e', 'h']
+    rev = rev.join(""); //convierte una array en una cadena: "olleh"
+    console.log(rev)
+
+    // TODO JUNTO SERÍA:
+
+    // function takesStr(str) {
+    //     let rev = str.split("");
+    
+    //     return str.reverse().join("");
+    //   }
+
+}
+
+takesStr("hello")
+
+
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+
+function newStr(string) {
+    let newString = string.split("");
+    let check = newString.reverse().join("");
+    
+    if(string === check) {
+        return "OH YEAH!"
+    } else {
+        return "Oops!"
+    }
+}
+
+console.log(newStr("heeeh"))
