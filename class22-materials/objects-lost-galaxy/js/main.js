3d2df4a0aa31675b4824e9f4b0c17140aec55819
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-01-27 14:51:17 +0100

    learning and practising with objects

--- class22-materials/objects-lost-galaxy/js/main.js
+++ class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,40 @@
 //Create a mouse object that has four properties and three methods
+
+let mouse = {}
+
+mouse.color = "black";
+mouse.shape = "round";
+mouse.brand = "trust"
+mouse.wired = true
+
+mouse.rigthClick = functon(click) {
+    console.log("right click")
+}
+
+mouse.leftClick = function(){
+    console.log("left click")
+}
+
+mouse.scroll = function() {
+    console.log("jump jump jump")
+}
+
+
+let mouse2 = {}
+
+mouse2.color = "black";
+mouse2.shape = "round";
+mouse2.brand = "logitech"
+mouse2.wired = false
+
+mouse2.rigthClick = functon(click) {
+    console.log("right click")
+}
+
+mouse2.leftClick = function(){
+    console.log("left click")
+}
+
+mouse2.scroll = function() {
+    console.log("jump jump jump")
+}
--- class22-materials/objects-street-fighter/js/main.js
+++ class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,21 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function MakeSTCharacter(characterName, characterPunch, catchPhrase, specialMove) {
+    this.name = characterName
+    this.puch = characterPunch
+    this.phrase = catchPhrase
+    this.special = specialMove
+
+    this.winning = function() {
+        console.log(`HAHAHA ${this.phrase}`)
+    }
+    this.dash = function() {
+        console.log('Oops you missed me!')
+    }
+    this.taunt = function() {
+        console.log(`You cannot handle my ${this.special}`)
+    }
+
+}
+
+let ryu = new MakeSTCharacter('Ryu', 'High', 'Get over here!!', 'HAYOOOUUKEENN!!')
