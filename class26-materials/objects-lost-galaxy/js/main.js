4e31adfa276428d19debff4cf7055022130409be
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-02-01 18:15:46 +0100

    adding new class

---
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -0,0 +1,21 @@
+//Create a pizza object that has four properties and three methods
+
+let pizza = {}
+
+pizza.shape = "round";
+pizza.size = "large"
+pizza.toppings = ['spinach', 'onion', 'jalapenos', 'cheese', 'garlic']
+pizza.place = "Dominos"
+
+pizza.deliveryTime = function() {
+        console.log("calculating...")
+}
+
+pizza.crunch = function() {
+    console.log(`mmm nice! it's crunchy!!! `)
+}
+
+pizza.burnMouth = function() {
+    console.log('zkdjfhasdflijhSLKIhjsd')
+}
+
