39b3ae8a8f9ff2c757c84140bd6c45f03aed25cc
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-02-06 13:02:44 +0100

    re-learning objects constructors

--- class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,38 @@
 //Create a constructor with 4 properties and 3 methods
+
+// function PizzaMachine (size, toppings, crust, sauce) {
+
+//     this.size = size
+//     this.toppings = toppings
+//     this.crust = crust
+//     this.sauce = sauce
+
+//     this.estimatedDeliveryTime = function() {
+//         console.log('Calculating...')
+//     }
+
+//     this.burnMouth = function() {
+//         console.log('FKJHSkhajKSHJkaS')
+//     }
+
+//     this.frisbee = function() {
+//         console.log('YYYYeeeee')
+//     }
+// }
+
+// let pizza = new PizzaMachine('large',['spinach', 'garlic'],'deep dish', 'white')
+
+class PizzaMachine {
+    constructor(size, toppings, crust, sauce) {
+        this.size = size
+        this.toppings = toppings
+        this.crust = crust
+        this.sauce = sauce
+    }
+    estimatedDeliveryTime() {
+        console.log('Calculating...')
+    }
+    burnMouth() {
+        console.log('FKJHSlakjSLKJ')
+    }
+}
