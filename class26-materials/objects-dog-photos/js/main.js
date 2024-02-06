12928f61836a371eb21f5066d2e303cc077fd4cc
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-02-06 15:10:20 +0100

    playing with APIs

--- class26-materials/objects-dog-photos/js/main.js
+++ class26-materials/objects-dog-photos/js/main.js
@@ -1,1 +1,19 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        document.querySelector('img').src = data.message
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+
+    //Lo que obtenemos como data se refleja en la consola,
+    // cuando abro esto en la consola veo que nos devuelve un objeto con 2 propiedades:
+        // La primera un mensaje con la URL y la segunda con status: 'success'
+        // Podemos utilizar data.message como URL
+
+        // Puedo introducir esto en DOM en el apartado de .then(data) en vez de utilizarlo el console.log
+        //document.querySelector('img').src = data.message
