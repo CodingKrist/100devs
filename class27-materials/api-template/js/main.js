bb9d71d43d3e1232c35aba8eb13b3a29b67c3bd9
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-02-08 16:05:32 +0100

    iterating with NASA API

--- class27-materials/api-template/js/main.js
+++ class27-materials/api-template/js/main.js
@@ -2,13 +2,20 @@
 document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const choice = document.querySelector('input').value.toLowerCase()
+  const url = 'https://api.nasa.gov/planetary/apod?api_key=12M9PL4WWCugYgfxHXRCsnjZPF5onGE0T83haBR3&date='+choice
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        if (data.media_type === 'image'){
+          document.querySelector('img').src = data.hdurl
+        } else if(data.media_type === 'video'){
+          document.querySelector('iframe').src = data.url
+          document.querySelector('img').classList.add('hidden')
+        }
+        document.querySelector('h3').innerText = data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
--- class27-materials/objects-netflix-tv-shows/js/main.js
+++ class27-materials/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,22 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class NetflixTvShow{
+    constructor(title, genre, rating, numOfEp) {
+        this.title = title;
+        this.genre = genre;
+        this.rating = rating
+        this.numOfEp = numOfEp;
+    }
 
+    play() {
+        console.log("Playing...")
+    }
+    stop() {
+        console.log("Stoping...")
+    }
+    saveToList() {
+        console.log("Saved to List")
+    }
+}
+
+let bridgerton = new NetflixTvShow('Bridgerton', 'Period Romantic Drama', '99%', 16)
