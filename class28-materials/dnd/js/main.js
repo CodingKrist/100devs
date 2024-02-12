4c84109dc3bdb68d1e78911eb9e2290f73828684
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-02-12 21:33:03 +0100

    d&d API, adding html to the DOM

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -8,7 +8,20 @@ function getFetch(){
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-       
+        console.log(data)
+        document.querySelector('h2').innerText = data.name
+        document.querySelector('h3').innerText = data.classes[0].name
+
+        document.querySelector('ul').innerText = '' //para borrar previamente si antes se habia añadido antes a la lista ul
+
+        data.subclasses.forEach(x => {
+            //create an li
+            const li = document.createElement('li')
+            //add text to li
+            li.textContent = x.name
+            //append the li to the ul
+            document.querySelector('ul').appendChild(li)
+        })
       })
       .catch(err => {
           console.log(`error ${err}`)
