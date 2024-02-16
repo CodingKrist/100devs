1bfe9b91fbc81cb4adf8d6fe32b553d2d0fab937
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-02-16 10:56:45 +0100

    learning Encapsulation and Abstraction

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,22 @@
-//Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+//Create an espresso machine class that makes machines with 4 properties and 3 methods
+
+class EspressoMachine {
+    constructor (color, make, model, price) {
+        this.color = color
+        this.make = make
+        this.model = model
+        this.price = price 
+    }
+    turnOn() {
+        console.log('I am on now!')
+    }
+    steam() {
+        console.log('Its summer SALE 50%')
+    }
+    brew() {
+        console.log('good stuff coming your way')
+    }
+
+}
+
+let gaggia = new EspressoMachine('red', 'Gaggia', 'Classic Pro', 400)
