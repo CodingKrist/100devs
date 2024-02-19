dc17c6c8cafac5012581a220bd7c7e38f5f26f27
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-02-19 15:09:57 +0100

    playing with Inheritance and Polymorfphism

--- class31-materials/class31-materials/objects-child-class/js/main.js
+++ class31-materials/class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,18 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
+class Animal {
+    constructor(name) {
+        this.name = name;
+        this.
+    }
+    speak() {
+        console.log(`${this.name} makes a sound`)
+    }
+}
+
+class Cat extends Animal {
+    constructor(name, breed){
+        super(name)
+        this.breed = breed
+    }
+}
