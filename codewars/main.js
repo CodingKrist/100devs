946cac4ba6d356ed653e6f0c8ff09551d1266969
CodingKrist <122282123+CodingKrist@users.noreply.github.com>
2024-02-21 11:27:03 +0100

    today's codewars

--- codewars/main.js
+++ codewars/main.js
@@ -1,11 +1,15 @@
-// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
 
-// Examples
-// "This is an example!" ==> "sihT si na !elpmaxe"
-// "double  spaces"      ==> "elbuod  secaps"
+// Example: (Input --> Output)
 
-let str = "double  spaces"
+// "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
 
-function reverseWords(str) {
-  return str.split(" ").map(x => x.split("").reverse().join("")).join(" ")
-}
+// isIsogram "Dermatoglyphics" = true
+// isIsogram "moose" = false
+// isIsogram "aba" = false
+
+let str = "moose"
+
+function isIsogram(str){
+ return new Set(str.toUpperCase()).size === str.length // new Set te hace un objeto con las letras de la cadena SIN REPETIR
+                                                        // por ejemplo de moose en el ejemplo hace: M O S E, un objeto con 4 elementos.
